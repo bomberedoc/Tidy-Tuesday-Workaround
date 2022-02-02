6d64b222d82d4459144b43149a43f547b4aca763
Anjan <gh.anjangh@gmail.com>
2022-02-02 19:12:38 +0530

    where is this?

---
+++ 02-11-2021.R
@@ -0,0 +1,9 @@
+# sudo apt install libgdal-dev
+install.packages('spData')
+install.packages('spDataLarge',
+                 repos = "https://nowosad.github.io/drat/", type = "source")
+# sudo apt install libudunits2-dev
+install.packages('sf')
+install.packages('raster')
+install.packages('tmap')
+install.packages('leaflet')
